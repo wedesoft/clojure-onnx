79c38d365690efc2cd651e352024b13759f0b583
Jan Wedekind (Dr) <jan@wedesoft.de>
2025-02-08 21:44:47 +0000

    Read training images and labels

--- train/train.py
+++ train/train.py
@@ -5,5 +5,9 @@ from torch.nn import functional as F
 
 
 def main():
-    train_images = np.fromfile('train-images-idx3-ubyte', dtype=np.uint8)
-    print(train_images)
+    # Read labels (skip magic and length integer)
+    train_labels = np.fromfile('train-labels-idx1-ubyte', dtype=np.uint8)[8:]
+    print(train_labels.shape)
+    # Read images (skip magic, length, height, and width integers)
+    train_images = np.fromfile('train-images-idx3-ubyte', dtype=np.uint8)[16:].reshape(-1, 28, 28)
+    print(train_images.shape)
