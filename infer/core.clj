f005fba0700879921850d24c0e50aa5d9f9888ec
Jan Wedekind (Dr) <jan@wedesoft.de>
2025-02-10 21:43:06 +0000

    Display first image from dataset

--- infer/core.clj
+++ infer/core.clj
@@ -1,7 +1,7 @@
 (ns infer.core
     (:require [clojure.java.io :as io]
               [cljfx.api :as fx])
-    (:import [java.io ByteArrayOutputStream]
+    (:import [java.io ByteArrayOutputStream ByteArrayInputStream]
              [javafx.application Platform]
              [ai.onnxruntime OrtEnvironment OrtSession]))
 
@@ -9,21 +9,32 @@
 
 (def mnist (-> environment (.createSession "mnist.onnx")))
 
-(def test-data
+(defn read-digit [n]
   (with-open [in (io/input-stream "data/t10k-images-idx3-ubyte")
               out (ByteArrayOutputStream.)]
-    (.skip in 16)
-    (io/copy in out)
+    (.skip in (+ 16 (* n 784)))
+    (.write out (.readNBytes in (* 28 28)))
     (.toByteArray out)))
+
+(defn digit->image [data]
+  (let [image  (java.awt.image.BufferedImage. 28 28 java.awt.image.BufferedImage/TYPE_BYTE_GRAY)
+        raster (.getRaster image)
+        out    (ByteArrayOutputStream.)
+        ]
+    (.setDataElements raster 0 0 28 28 data)
+    (javax.imageio.ImageIO/write image "png" out)
+    (.flush out)
+    (javafx.scene.image.Image. (ByteArrayInputStream. (.toByteArray out)))))
+
+; https://gist.github.com/jamesthompson/3344090
 
 (def app-state (atom {:image "debian.png"}))
 
 (defn event-handler [& args]
-  (swap! app-state update :image (constantly "debian2.png")))
+  (swap! app-state update :image (constantly (digit->image (read-digit 0)))))
 
 (defn display-image [{:keys [image]}]
   {:fx/type :image-view
-   ; :image (javafx.scene.image.Image. "debian.png")
    :fit-width 256
    :fit-height 256
    :image image})
